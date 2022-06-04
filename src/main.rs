3c726758f368549affe715fb89fcc9f5e2ab43c1
Sugi <sugidaffection@gmail.com>
2022-06-05 02:55:31 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@ use actix_session::{storage, SessionMiddleware};
 use actix_web::{cookie, middleware, web, App, HttpServer};
 
 use dotenv::dotenv;
-use shortlink::AppData;
-use shortlink::{auth, home, user};
+use shortlink::{auth, create_pool, home, user, AppData};
 use std::env;
+
+#[macro_use]
+extern crate diesel_migrations;
+use diesel_migrations::embed_migrations;
+embed_migrations!();
+
+async fn run_db_migrations() {
+    let conn = create_pool(None).get().unwrap();
+    embedded_migrations::run(&*conn).expect("failed to run migrations")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    run_db_migrations().await;
     HttpServer::new(|| {
         let key = env::var("SECRET_KEY").map_or(cookie::Key::generate(), |x| {
             cookie::Key::derive_from(x.as_bytes())
