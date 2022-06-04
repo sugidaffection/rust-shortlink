7d9b7334930edeccd0e29a4619ac84ef97cc3b0d
Sugi <sugidaffection@gmail.com>
2022-06-05 00:35:34 +0700

    fix remember checkbox value

--- src/forms.rs
+++ src/forms.rs
@@ -1,11 +1,21 @@
 use secrecy::Secret;
-use serde::Deserialize;
+use serde::{de, Deserialize};
 
 #[derive(Clone, Deserialize)]
 pub struct LoginForm {
     pub email: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_bool")]
     pub remember: bool,
+}
+
+fn deserialize_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let s: &str = de::Deserialize::deserialize(deserializer).unwrap_or("n");
+
+    Ok(["on", "yes", "true", "1", "checked", "y"].contains(&s))
 }
 
 #[derive(Clone, Deserialize)]
