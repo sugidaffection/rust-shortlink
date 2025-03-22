0a613f6da0a0b2c4f6f5bdb29fe6f613ca38cf4a
Sugi <developer.sugiono@gmail.com>
2025-03-22 18:17:33 +0700

    fix: handle dotenv loading error gracefully

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> Result<(), AppError> {
     use shortlink::{api, prelude::*};
     use std::env;
 
-    dotenv().map_err(AppError::EnvLoadError)?;
+    dotenv().ok();
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
